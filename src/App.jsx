84e55e7d8a98e51a518239c18eac9cac8553e391
T-126 <rathnayaka3548@gmail.com>
2023-11-05 10:01:23 +0530

    MAIN Added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+// --------------------------------------------
+import Card from "./components/Card.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+}
---
+++ src/components/headcont/HeadCont.jsx
@@ -0,0 +1,11 @@
+import Button from "./Buttons";
+
+export default function HeadCont() {
+  return (
+    <div className="head-text">
+      <h5 className="card-title-main">T-126</h5>
+      <h6 className="card-subtitle fw-light">Student</h6>
+      <Button />
+    </div>
+  );
+}
